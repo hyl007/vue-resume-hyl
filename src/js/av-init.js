b023c1e279c6abde2f141c437c32a1830b632ea0
hyl <314773165@qq.com>
2018-06-04 14:58:50 +0800

    vue-resume

--- src/js/av-init.js
+++ src/js/av-init.js
@@ -1,8 +1,7 @@
-{
-    let APP_ID = '2Ju8DXu7Wz0YiIBQrDPCdygo-gzGzoHsz';
-    let APP_KEY = 'hrHwWFXvFbsN4AYrQrzvpnjw';
+
+    var APP_ID = '2Ju8DXu7Wz0YiIBQrDPCdygo-gzGzoHsz';
+    var APP_KEY = 'hrHwWFXvFbsN4AYrQrzvpnjw';
     AV.init({
         appId: APP_ID,
         appKey: APP_KEY
     });
-}
